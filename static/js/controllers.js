94c33cb9b0437985f64a03e678e27b3772930507
Jess Zanewich <jessica.zanewich@gmail.com>
2015-06-11 22:42:47 -0700

    Displaying certain information about the movie, auto refresh as user types title in search box

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,60 +1,48 @@
 angular.module("MediaSearchControllers", [])
 
 .controller('SearchCtrl', function($scope, $http) {
-	var search_term;
+	
 
 	if($scope.search === undefined) {
-		$scope.search = "Sherlock Holmes";
-		//findMedia();
+		$scope.search = "";
+		fetch();
 	}
 
+	var search_term;
 	$scope.change = function() {
-		findMedia();
-		/*if(search_term) {
+		if(search_term) {
 			clearTimeout(search_term); //User has continued typing
 		}
-
-		search_term = setTimeout(findMedia, 1200);*/ //User has stopped typing for 800miliseconds, so search begins
+		search_term = setTimeout(fetch, 800);//User has stopped typing for 800miliseconds, so search begins
 	}
 
-	function findMedia() {
-		//Search based on 'http://www.omdbapi.com/' specifications
-		$http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true").
-		success(function(data) {
-			$scope.information = data;
-			console.log(data);
-		});
-		$http.get("http://www.omdbapi.com/?s=" + $scope.search).
-		success(function(data) {
-			$scope.search = data;
-			console.log(data);
-		});
+	//Choosing from dropdown
+	/*$scope.listChoice = function(list_search) {
+		
 	}
 
 	function selectMedia() {
 		if(!$scope.search) {
 			console.log("Please type something in the search");
 		};
-	}
+	}*/
 
-	function searchData(search) {
-		$scope.search = search;
-		$http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true").
+	function fetch() {
+		//Search based on 'http://www.omdbapi.com/' specifications
+		$http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full").
 		success(function(data) {
-			$scope.information = data;
-			console.log(data);
+			$scope.details = data;
 		});
 		$http.get("http://www.omdbapi.com/?s=" + $scope.search).
 		success(function(data) {
-			$scope.search = data;
-			console.log(data);
+			$scope.related = data;
 		});
 	}
 
-	/*$scope.retrieve = function(movie) {
-		$scope.titles = movie.Title;
+	$scope.update = function(movie) {
+		$scope.search = movie.Title;
 		$scope.change();
-	}*/
+	}
 
 	$scope.select = function(){
   		this.setSelectionRange(0, this.value.length);
---
+++ static/js/directives.js
@@ -0,0 +1,15 @@
+/*angular.module("MediaSearchDirectives", [])
+	
+	.directive("mediaSelectDirective", function() {
+		return {
+			restrict: "E",
+			require: '^ngClick',
+			scope: {
+				ngClick: '='
+			},
+			template: '<button>'
+			link: function(scope, element, attrs) {
+
+			}
+		}
+	})*/
